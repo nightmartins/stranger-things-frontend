4fbe02389a0fbf8e7359390a6a575bb7301ca202
natalia <mellomartins.naty@gmail.com>
2022-03-11 09:06:41 -0300

    correçao variaveis de ambiente

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const developmentVersion = process.env.DEVELOPMENT_VERSION === 'true'
+const developmentVersion = process.env.REACT_APP_DEVELOPMENT_VERSION === 'true'
   ? 'Em desenvolvimento' : ' ';
 
 function App() {
