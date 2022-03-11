1c2757f2c8d8f2ff27bce108ab78614fa3679ee2
natalia <mellomartins.naty@gmail.com>
2022-03-10 22:33:55 -0300

    development remote

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const developmentVersion = process.env.DEVELOPMENT_VERSION === 'true'
+  ? <h1>Em desenvolvimento</h1> : <p> </p>;
+
 function App() {
   return (
     <div className="App">
       <StrangerThings />
+      <h1>{ developmentVersion }</h1>
     </div>
   );
 }
